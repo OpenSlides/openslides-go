fdd60c453489691c1b5e93b09e673acc2375ae45
Bastian Rihm <brihm@intevation.de>
2026-03-23 18:40:18 +0100

    Skip non existing fields on collect fields

--- datastore/flow_postgres.go
+++ datastore/flow_postgres.go
@@ -374,11 +374,9 @@ func createKeyList(collection string, id int, fields []string) ([]dskey.Key, err
 	}
 
 	keys := make([]dskey.Key, len(fields))
-	var err error
 	for i, field := range fields {
-		keys[i], err = dskey.FromParts(collection, id, field)
-		if err != nil {
-			return nil, fmt.Errorf("creating key from parts %q, %d, %q: %w", collection, id, field, err)
+		if key, err := dskey.FromParts(collection, id, field); err == nil {
+			keys[i] = key
 		}
 	}
 	return keys, nil
